ca6ac1a9a0f23b2dc5d3a9ac602aff7d96a46c65
mtganyu <mtganyu@gmail.com>
2018-05-03 13:08:22 +0200

    modified init sql so it works now

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,27 +1,25 @@
-﻿/*
+﻿﻿/*
     Database initialization script that runs on every web-application redeployment.
 */
 DROP TABLE if EXISTS slot_tasks;
 DROP TABLE IF EXISTS task;
 DROP TABLE IF EXISTS slot;
-DROP TABLE IF EXISTS schedule;
 DROP TABLE IF EXISTS col;
+DROP TABLE IF EXISTS schedule;
 DROP TABLE IF EXISTS users;
 
 
 
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     role TEXT NOT NULL
 );
 
-
-
 CREATE TABLE schedule (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
 	name TEXT NOT NULL,
 	col_id INTEGER,
@@ -39,7 +37,7 @@ CREATE TABLE col (
 
 
 CREATE TABLE slot (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     col_id INTEGER NOT NULL,
     start INTEGER NOT NULL CHECK (start>=0 AND start <24),
     stop INTEGER NOT NULL CHECK (stop>=0 AND stop <24),
@@ -47,7 +45,7 @@ CREATE TABLE slot (
 );
 
 CREATE TABLE task (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     description TEXT NOT NULL,
     user_id INTEGER NOT NULL,
     FOREIGN KEY(user_id)REFERENCES users(id)
@@ -64,24 +62,24 @@ CREATE TABLE slot_tasks(
 
 
 INSERT INTO users (id,name,email,role) VALUES
-	(1,'user1','user1@user1','ADMIN'), -- 1
-	(2,'user2','user2@user2','REGISTERED'), -- 2
-	(3,'user3','user3@user3','GUEST'); -- 3
+	(001,'user1','user1@user1','ADMIN'), -- 1
+	(002,'user2','user2@user2','REGISTERED'), -- 2
+	(003,'user3','user3@user3','GUEST'); -- 3
 
 INSERT INTO col(id,name)VALUES
     (1,'Pisti'),
     (2,'Itvan');
 
-INSERT INTO schedule (id,user_id,name, col_id) VALUES
-	(1, 1, 'napirend', 1),   -- 1
-	(2 , 2, 'blabla', 1),-- 2
-	(3 , 3, 'blabla2', 2);-- 2
+INSERT INTO schedule (id,user_id,name) VALUES
+	(1,001,'napirend'),   -- 1
+	(2 ,002,'blabla'),-- 2
+	(3 ,003,'blabla2');-- 2
 
 
 
 INSERT INTO slot(id,col_id,start,stop)VALUES
-    (1,1,1,2),
-    (2,2,3,4);
+    (1,01,1,2),
+    (2,02,3,4);
 
 
 
