55e6793fb8b95541a73ecd4abf4137531fd8be1d
Balint Csizmadia <balintcsizmadia1@gmail.com>
2018-05-03 13:25:51 +0200

    fix insert problem

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,4 +1,4 @@
-﻿﻿/*
+﻿/*
     Database initialization script that runs on every web-application redeployment.
 */
 DROP TABLE if EXISTS slot_tasks;
@@ -9,35 +9,32 @@ DROP TABLE IF EXISTS schedule;
 DROP TABLE IF EXISTS users;
 
 
-
-
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     role TEXT NOT NULL
 );
 
 CREATE TABLE schedule (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
 	name TEXT NOT NULL,
 	col_id INTEGER,
 	FOREIGN KEY(user_id)REFERENCES users(id)
 );
+
+
 CREATE TABLE col (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
 	schedule_id INTEGER ,
 	FOREIGN KEY(schedule_id) REFERENCES schedule(id)
 );
 
 
-
-
-
 CREATE TABLE slot (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     col_id INTEGER NOT NULL,
     start INTEGER NOT NULL CHECK (start>=0 AND start <24),
     stop INTEGER NOT NULL CHECK (stop>=0 AND stop <24),
@@ -45,13 +42,13 @@ CREATE TABLE slot (
 );
 
 CREATE TABLE task (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     description TEXT NOT NULL,
     user_id INTEGER NOT NULL,
     FOREIGN KEY(user_id)REFERENCES users(id)
 );
 
-CREATE TABLE slot_tasks(
+CREATE TABLE slot_tasks (
     slot_id INTEGER,
     task_id INTEGER,
     FOREIGN KEY(slot_id)REFERENCES slot(id),
@@ -61,32 +58,28 @@ CREATE TABLE slot_tasks(
 
 
 
-INSERT INTO users (id,name,email,role) VALUES
-	(001,'user1','user1@user1','ADMIN'), -- 1
-	(002,'user2','user2@user2','REGISTERED'), -- 2
-	(003,'user3','user3@user3','GUEST'); -- 3
+INSERT INTO users (name, email, role) VALUES
+	('user1','user1@user1','ADMIN'), -- 1
+	('user2','user2@user2','REGISTERED'), -- 2
+	('user3','user3@user3','GUEST'); -- 3
 
-INSERT INTO col(id,name)VALUES
-    (1,'Pisti'),
-    (2,'Itvan');
+INSERT INTO col(name)VALUES
+    ('Pisti'),
+    ('Itvan');
 
-INSERT INTO schedule (id,user_id,name) VALUES
-	(1,001,'napirend'),   -- 1
-	(2 ,002,'blabla'),-- 2
-	(3 ,003,'blabla2');-- 2
-
-
-
-INSERT INTO slot(id,col_id,start,stop)VALUES
-    (1,01,1,2),
-    (2,02,3,4);
+INSERT INTO schedule (user_id, name) VALUES
+	(1,'napirend'),
+	(2,'blabla'),
+	(3,'blabla2');
 
 
 
-INSERT INTO task(id,description,user_id)VALUES
-    (1,'kutya simogatas',001),
-    (2,'kutya setaltatas',002);
+INSERT INTO slot(col_id,start, stop) VALUES
+    (1,1,2),
+    (2,3,4);
 
 
-
-
+INSERT INTO task(description, user_id) VALUES
+    ('kutya simogatas', 1),
+    ('kutya setaltatas', 2);
+S
