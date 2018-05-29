be7af64bf98882e78263b51d16eff1e2d74f5738
zsarna2 <zsarna2@gmail.com>
2018-05-29 13:59:29 +0200

    schedule drag n drop readDropZone function reimplemented, now its working

--- src/main/webapp/schedule.js
+++ src/main/webapp/schedule.js
@@ -24,14 +24,14 @@ function onScheduleResponse() {
     }
 }
 
-function onHowManyResponse(){
+function onHowManyResponse() {
     if (this.status === OK) {
-            clearMessages();
-            showContents(['schedule-tasks-content']);
-            onTasksReceived(JSON.parse(this.responseText));
-        } else {
-            onOtherResponse(schedulesContentDivEl, this);
-        }
+        clearMessages();
+        showContents(['schedule-tasks-content']);
+        onTasksReceived(JSON.parse(this.responseText));
+    } else {
+        onOtherResponse(schedulesContentDivEl, this);
+    }
 }
 
 function onHowManyColClicked() {
@@ -53,25 +53,25 @@ function createTasksTableBody(tasks) {
         const objTdEl = document.createElement('td');
 
         // creating name cell
-        const dataUserIdAttr = document.createAttribute('data-task-id');
+        const dataUserIdAttr = document.createAttribute('id');
         dataUserIdAttr.value = task.id;
 
 
-	const objDivEl = document.createElement('div');
-	objDivEl.setAttribute('class', "objects");
-    objDivEl.textContent = task.name;
-    objDivEl.setAttributeNode(dataUserIdAttr);
-	objDivEl.setAttribute('draggable', true);
-	objDivEl.setAttribute('ondragstart', 'drag_start(event)');
-	objDivEl.setAttribute('ondragend', 'drag_end(event)');
+        const objDivEl = document.createElement('div');
+        objDivEl.setAttribute('class', "objects");
+        objDivEl.textContent = task.name;
+        objDivEl.setAttributeNode(dataUserIdAttr);
+        objDivEl.setAttribute('draggable', true);
+        objDivEl.setAttribute('ondragstart', 'drag_start(event)');
+        objDivEl.setAttribute('ondragend', 'drag_end(event)');
         objDivEl.addEventListener('click', onLoadTasks);
 
 
 
         // creating row
-    const trEl = document.createElement('tr');
-	objTdEl.appendChild(objDivEl);
-	trEl.appendChild(objTdEl);
+        const trEl = document.createElement('tr');
+        objTdEl.appendChild(objDivEl);
+        trEl.appendChild(objTdEl);
         tbodyEl.appendChild(trEl);
     }
 
@@ -115,23 +115,28 @@ function createTasksTable(tasks) {
     return tableEl;
 }
 
-function createDropTableBody(dropTableElId){
-    const tbodyEl = document.createElement('tbody');
-    const dropTableEl = document.getElementById('colTable');
-
+function createDropTableBody(dropTableElId) {
     const getDataButtonEl = document.createElement('button');
     const clearButtonEl = document.createElement('button');
+    getDataButtonEl.id = dropTableElId;
+    clearButtonEl.id = dropTableElId;
+
+    const tbodyEl = document.createElement('tbody');
+    tbodyEl.className = dropTableElId;
+    const dropTableEl = document.getElementById('colTable');
 
     let counter = 0;
-    while(counter < 24){
+    while (counter < 24) {
         const dropTableEl = document.getElementById('colTable');
         const dropTrEl = document.createElement('tr');
         const dropTdEl = document.createElement('td');
 
         // CREATE DROPZONE (SLOT) WITH IT'S ATTRIBUTES
         const dropDivEl = document.createElement('div');
-        dropDivEl.id = dropTableElId+counter;
+        dropDivEl.id = dropTableElId + counter;
         dropDivEl.className = 'drop_zone';
+        dropDivEl.value = counter + 1;                          //this is muy importante
+        dropDivEl.textContent = dropDivEl.value;                 //you can leave this line after the css version is implemented
         const dropDivElId = dropDivEl.id;
         dropDivEl.setAttribute('ondrop', 'drag_drop(event)');
         dropDivEl.setAttribute('ondragover', 'return false');
@@ -144,37 +149,38 @@ function createDropTableBody(dropTableElId){
         //clearButtonEl.setAttribute('drop_zone', dropTableElId+(counter+2));
 
 
-
         dropTdEl.appendChild(dropDivEl);
 
         dropTrEl.appendChild(dropTdEl);
         tbodyEl.appendChild(dropTrEl);
+
         counter++;
-        }
+    }
 
-        getDataButtonEl.addEventListener('click', function() {
-            readDropZone(dropTableElId);
-        });
-        getDataButtonEl.textContent = 'Get Data';
-        clearButtonEl.addEventListener('click', function() {
-            clearDropZone(dropTableElId);
-        });
-        clearButtonEl.textContent = 'Clear';
+    getDataButtonEl.addEventListener('click', function () {
+        readDropZone(tbodyEl, dropTableElId);                       //function calling arguments has changed
+    });
+    getDataButtonEl.textContent = 'Save';
+    clearButtonEl.addEventListener('click', function () {
+        clearDropZone();
+    });
+    clearButtonEl.textContent = 'Clear';
 
-        tbodyEl.appendChild(getDataButtonEl);
-        tbodyEl.appendChild(clearButtonEl);
+    tbodyEl.appendChild(getDataButtonEl);
+    tbodyEl.appendChild(clearButtonEl);
 
     return tbodyEl;
 }
 
 function createDropTableHead(dropTableElId) {
+
     const theadEl = document.createElement('thead');
     const dropTableEl = document.getElementById('colTable');
     const dropTrEl = document.createElement('tr');
     const dropTdEl = document.createElement('td');
 
     const tnameEl = document.createElement('input');
-    tnameEl.id = dropTableElId+'nameIn';
+    tnameEl.id = dropTableElId + 'nameIn';
     tnameEl.className = "schedule-name-input";
 
     dropTdEl.appendChild(tnameEl);
@@ -184,30 +190,30 @@ function createDropTableHead(dropTableElId) {
     return theadEl;
 }
 
-function createDropZone(){
+function createDropZone() {
     const scheduleFormEl = document.forms['how-many'];
     const nameInputEl = scheduleFormEl.querySelector('input[name="howMany"]');
     const number = nameInputEl.value;
     let dayCounter = 0;
 
     const divEl = document.getElementById('task-list-drop');
-    for(let i = 0; i < number; i++){
-    const dropTableEl = document.createElement('table');
-    dropTableEl.setAttribute("id", "colTable"+i);
-    dropTableEl.style.cssFloat = 'right';
-    const dropTableElId = dropTableEl.id;
-    const dropTableBodyEl = createDropTableBody(dropTableElId);
-    const dropTableHeadEl = createDropTableHead(dropTableElId);
-    dropTableEl.appendChild(dropTableHeadEl);
-    dropTableEl.appendChild(dropTableBodyEl);
-    divEl.appendChild(dropTableEl);
+    for (let i = 0; i < number; i++) {
+        const dropTableEl = document.createElement('table');
+        dropTableEl.setAttribute("id", "colTable" + i);
+        dropTableEl.style.cssFloat = 'right';
+        const dropTableElId = dropTableEl.id;
+        const dropTableBodyEl = createDropTableBody(dropTableElId);
+        const dropTableHeadEl = createDropTableHead(dropTableElId);
+        dropTableEl.appendChild(dropTableHeadEl);
+        dropTableEl.appendChild(dropTableBodyEl);
+        divEl.appendChild(dropTableEl);
     }
 
     return dropTableEl;
 }
 
-function _(id){
-   return document.getElementById(id);
+function _(id) {
+    return document.getElementById(id);
 }
 
 function returnFalse() {
@@ -217,88 +223,62 @@ function returnFalse() {
 function drag_start(event) {
     currDiv = event.target;
     event.dataTransfer.dropEffect = "move";
-    event.dataTransfer.setData("text", event.target.textContent );
+    event.dataTransfer.setData("text", event.target.textContent);
 }
 
 function drag_drop(event) {
+    event.target.textContent = null;                                //remove this after the css implementaion
     event.preventDefault();
     event.target.appendChild(currDiv);
-    currDiv.removeAttribute("draggable");
+    //currDiv.removeAttribute("draggable");
     currDiv.style.cursor = "default";
     droppedIn = true;
+
+    checkDropBoxes();                                               //remove this after the css implementaion
 }
 
 function drag_end(event) {
-	droppedIn = false;
+    droppedIn = false;
 }
-function readDropZone(dropTableElId){
-    let queryForAll;
-    let queryForDropZone;
-    switch(dropTableElId){
-    case 'colTable0' :
-        queryForAll = '[id^=colTable0]';
-        break;
-    case 'colTable1' :
-        queryForAll = '[id^=colTable1]';
-        break;
-    case 'colTable2' :
-        queryForAll = '[id^=colTable2]';
-        break;
-    case 'colTable3' :
-        queryForAll = '[id^=colTable3]';
-        break;
-    case 'colTable4' :
-        queryForAll = '[id^=colTable4]';
-        break;
-    case 'colTable5' :
-        queryForAll = '[id^=colTable5]';
-        break;
-    case 'colTable6' :
-        queryForAll = '[id^=colTable6]';
-        break;
+function readDropZone(tbodyEl, dropTableElId) {
+    const dayName = document.getElementById(dropTableElId + 'nameIn')
+    let tasks = new Map();
+    for (var j = 0; j < tbodyEl.children.length - 2; j++) {
+        let taskObj = tbodyEl.children[j].children["0"].children["0"].children["0"];
+        let taskStart = tbodyEl.children[j].children["0"].children["0"].value;
+        if (tbodyEl.contains(taskObj)) {
+            tasks.set(taskObj.id, taskStart);
+        }
     }
-
-    let bigParent = document.querySelectorAll(queryForAll);
-    for(var j=0; j < bigParent.length; j++){
-        for(var i=0; i < bigParent[j].children.length; i++){
-            alert(bigParent[j].children[i].textContent+" is in the drop zone");
+    const data = JSON.stringify({ "dropTableElId": dropTableElId, "dayName": dayName, "tasks": tasks });
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onTaskReloadResponse);
+    xhr.addEventListener('error', onNetworkError);
+    xhr.open('PUT', 'protected/schedule');
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(data);
+}
+function clearDropZone() {
+    const columntables = document.getElementsByClassName('columntable');
+    for (let j = 0; j < columntables.length; j++) {
+        for (let i = 0; i < columntables[j].children.length; i++) {
+            let delObj = columntables[j].children[i].children["0"].children["0"].children["0"];
+            if (delObj !== null) {
+                delObj.remove();
+                checkDropBoxes();
+            }
         }
     }
 }
-function clearDropZone(dropTableElId){
-    let queryForAll;
-    let queryForDropZone;
-    switch(dropTableElId){
-    case 'colTable0' :
-        queryForAll = '[id^=colTable0]';
-        break;
-    case 'colTable1' :
-        queryForAll = '[id^=colTable1]';
-        break;
-    case 'colTable2' :
-        queryForAll = '[id^=colTable2]';
-        break;
-    case 'colTable3' :
-        queryForAll = '[id^=colTable3]';
-        break;
-    case 'colTable4' :
-        queryForAll = '[id^=colTable4]';
-        break;
-    case 'colTable5' :
-        queryForAll = '[id^=colTable5]';
-        break;
-    case 'colTable6' :
-        queryForAll = '[id^=colTable6]';
-        break;
+
+function checkDropBoxes() {
+    const columntables = document.getElementsByClassName('columntable');
+    for (let j = 0; j < columntables.length; j++) {
+        for (let i = 0; i < columntables[j].children.length; i++) {
+            if (columntables[j].children[i].children["0"].children["0"].textContent === "") {
+                columntables[j].children[i].children["0"].children["0"].textContent = columntables[j].children[i].children["0"].children["0"].value;
+            }
+        }
     }
-    let bigParent = document.querySelectorAll(queryForAll);
-    for(var j=0; j < bigParent.length; j++){
-        for(var i=0; i < bigParent[j].children.length; i++){
-		    let delObj = bigParent[j].children[0];
-		    if(bigParent[j].firstChild){
-			    delObj.parentNode.removeChild(delObj);
-			}
-		}
-	}
 }
 
