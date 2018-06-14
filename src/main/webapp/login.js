43b925609545d4e4c72c0f64962a233e7279a258
Balint Csizmadia <balintcsizmadia1@gmail.com>
2018-06-14 09:01:12 +0200

    fix profile picture multiplication

--- src/main/webapp/login.js
+++ src/main/webapp/login.js
@@ -36,22 +36,20 @@ function toRegisterButtonClick() {
 }
 
 function onSignIn(googleUser) {
+    const id_token = googleUser.getAuthResponse().id_token;
+    const usrPic = googleUser.w3.Paa;
+    const pic = document.createElement("img");
+    const picDivEl = document.getElementById("profilePicture");
+    removeAllChildren(picDivEl);
+    picDivEl.appendChild(pic);
+    pic.src = usrPic;
 
-
-  const id_token = googleUser.getAuthResponse().id_token;
-  const usrPic = googleUser.w3.Paa;
-  const pic = document.createElement("img");
-  const picDivEl = document.getElementById("profilePicture");
-  picDivEl.appendChild(pic);
-  pic.src = usrPic;
-
-
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', onGoogleLoginResponse);
-  xhr.addEventListener('error', onNetworkError);
-  xhr.open('POST', 'googlelogin');
-  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhr.send('idtoken=' + id_token);
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onGoogleLoginResponse);
+    xhr.addEventListener('error', onNetworkError);
+    xhr.open('POST', 'googlelogin');
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send('idtoken=' + id_token);
 }
 
 function onGoogleLoginResponse() {
